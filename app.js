fc1c97e1ec17579291f131786089ead2e78f4884
MartynasT <37385446+MartynasT@users.noreply.github.com>
2021-04-13 22:15:32 +0300

    init

---
+++ app.js
@@ -0,0 +1,62 @@
+const mainHolder = document.getElementById('main');
+const images = document.querySelectorAll('img');
+
+//LIGHTBOX
+const lightbox = document.createElement('div');
+const leftArrow = document.createElement('i');
+const rightArrow = document.createElement('i');
+let img = document.createElement('img');
+
+lightbox.classList.add('lightbox');
+leftArrow.classList.add('las', 'la-chevron-left');
+rightArrow.classList.add('las', 'la-chevron-right');
+lightbox.appendChild(leftArrow);
+lightbox.appendChild(img);
+lightbox.appendChild(rightArrow);
+
+images.forEach(img => {
+  img.addEventListener('click', ()=>{
+    showLightBox(img);
+  })
+});
+
+leftArrow.addEventListener('click', ()=>{
+  const currentSrc = img.src;
+  images.forEach((item, index)=> {
+    if (item.src === currentSrc) {
+      if (index === 0) {
+        img.src = images[8].src;
+      } else {
+        img.src = images[index - 1].src;
+      }
+    }
+  })
+});
+
+rightArrow.addEventListener('click', ()=>{
+  const currentSrc = img.src;
+  images.forEach((item, index)=> {
+    if (item.src === currentSrc) {
+      if (index === 8) {
+        img.src = images[0].src;
+      } else {
+        img.src = images[index + 1].src;
+      }
+    }
+  })
+})
+
+function showLightBox(item){
+  mainHolder.appendChild(lightbox);
+  img.src = item.src;
+}
+
+document.addEventListener('click', function(event) {
+  if (event.target.closest('.main')) return;
+  lightbox.remove();
+}, false);
+
+
+
+// TODO: add some animations
+// TODO: make it responsive
